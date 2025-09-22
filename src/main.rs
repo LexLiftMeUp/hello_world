fa83e279b596b05c0bb83d6fff68ff28b5ded01f
Abraham <abraham.v2003.av@gmail.com>
2025-09-21 22:42:25 -0400

    chapter 10 challenge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use std::env;
-use std::fs;
-fn main () {
-    if env::args ().len() < 2 {
-    eprintln! ("Program requires two arguments: <file path> <search name>");
-    std::process::exit(1);
-    }
-    let file_path = env::args () .nth (1) . unwrap ();
-    let search_name = env::args ().nth (2) .unwrap ();
-    for line in fs::read_to_string(file_path).unwrap().lines() {
-    if line == search_name {
-    println! ("{} did walk on the Moon!", search_name);
-    return;
-    }
-    }
-    println! ("{} did NOT walk on the Moon... YET!", search_name);
-
+struct Rectangle {width: f64, height: f64
 }
- 
+
+impl Rectangle {
+    fn get_area(&self) -> f64 {
+        self.width * self. height
+    }
+
+    fn scale (&mut self, scalar: f64) {
+        self.width *= scalar;
+        self.height *= scalar;
+    }
+    fn new(width: f64, height: f64) -> Rectangle {
+        Rectangle {
+            width,
+            height
+        }
+    }
+}
+fn main() {
+let mut rect = Rectangle:: new(1.2, 3.4);
+assert_eq!(rect.get_area(), 4.08);
+rect.scale (0.5);
+assert_eq!(rect.get_area(), 1.02);
+println!("Tests passed!");
+}

37cad6f49f9ae2703c63aa8ab12f622ad1307530
Abraham <abraham.v2003.av@gmail.com>
2025-09-21 21:55:08 -0400

    chapter 9 challenge

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-use rand::prelude::*;
-use std::io;
-fn main() {
-
-    let number = thread_rng().gen_range(1..101);
-    println!("Guess a number between 1 and 100");
-
-     
-
-    loop {
-        let mut buffer = String::new();
-
-    io::stdin().read_line(&mut buffer);
-    
-    let ints = buffer.trim().parse::<i32>().expect("Invalid input: Please enter a valid integer");
-        if ints < number{
-            println!("Guess higher");
-        }
-        else if ints > number{
-            println!("guess lower");
-        }
-        else{
-            println!("You got it!");
-            break;
-        }
-        
+use std::env;
+use std::fs;
+fn main () {
+    if env::args ().len() < 2 {
+    eprintln! ("Program requires two arguments: <file path> <search name>");
+    std::process::exit(1);
     }
-    println!("The number was {}!", number);
+    let file_path = env::args () .nth (1) . unwrap ();
+    let search_name = env::args ().nth (2) .unwrap ();
+    for line in fs::read_to_string(file_path).unwrap().lines() {
+    if line == search_name {
+    println! ("{} did walk on the Moon!", search_name);
+    return;
+    }
+    }
+    println! ("{} did NOT walk on the Moon... YET!", search_name);
 
 }
  

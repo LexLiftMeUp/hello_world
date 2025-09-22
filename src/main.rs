c077a680585eb5ce1e3ca614ab4721411a3a58ce
Abraham <abraham.v2003.av@gmail.com>
2025-09-22 17:37:47 -0400

    lab 4 Question 2 Solution

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-struct Rectangle {width: f64, height: f64
+struct Rectangle {
+width: f64,
+height: f64,
+}
+impl Rectangle {
+fn new(width: f64, height: f64) -> Rectangle {
+Rectangle { width, height }
+}
+fn area(&self) -> f64 {
+self.height * self.width
 }
 
-impl Rectangle {
-    fn get_area(&self) -> f64 {
-        self.width * self. height
-    }
-
-    fn scale (&mut self, scalar: f64) {
-        self.width *= scalar;
-        self.height *= scalar;
-    }
-    fn new(width: f64, height: f64) -> Rectangle {
-        Rectangle {
-            width,
-            height
-        }
-    }
+fn perimeter(&self) -> f64 {
+2.0*self.height + 2.0*self.width
+}
+fn is_square(&self) -> bool {
+if self.height == self.width {
+    return true
+}
+else {
+    return false
+}
+}
 }
 fn main() {
-let mut rect = Rectangle:: new(1.2, 3.4);
-assert_eq!(rect.get_area(), 4.08);
-rect.scale (0.5);
-assert_eq!(rect.get_area(), 1.02);
-println!("Tests passed!");
+let rect = Rectangle::new(5.0, 5.0);
+println!("Area: {}", rect.area());
+println!("Perimeter: {}", rect.perimeter());
+println!("Is square? {}", rect.is_square());
+assert!(Rectangle::new(5.0, 5.0).is_square());
+assert!(!Rectangle::new(5.0, 6.0).is_square());
 }
